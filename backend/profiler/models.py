7b7783f09944ed75235bb601892cd5b7be6f75a6
aadarshv3 <aadarshv3@gmail.com>
2024-10-07 10:24:19 +0000

    Add and register user models

--- backend/profiler/models.py
+++ backend/profiler/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.username} - {self.first_name} {self.last_name}"
