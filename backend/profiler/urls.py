81e099f3ed83caacbf238a6d908ebda5d2da2bb5
nailsonseat <aadarshv3@gmail.com>
2024-10-07 21:53:37 +0530

    registered url in app as well as the project

---
+++ backend/profiler/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views.userProfile import UserProfileViewSet
+
+router = DefaultRouter()
+router.register(r'profiles', UserProfileViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
