9ef9ad652aabac8a9b47cc2e52f77e511891e483
nailsonseat <aadarshv3@gmail.com>
2024-10-08 12:24:28 +0530

    enabled cors

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'profiler'
 ]
 
@@ -30,6 +31,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:33535',
 ]
 
 ROOT_URLCONF = 'server.urls'
