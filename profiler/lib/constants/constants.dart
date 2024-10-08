bcf4389dcf18e06f6bfcd67093709402a245142c
nailsonseat <aadarshv3@gmail.com>
2024-10-09 00:42:50 +0530

    update constants

--- profiler/lib/constants/constants.dart
+++ profiler/lib/constants/constants.dart
@@ -1,7 +1,6 @@
-
 class AppConstants {
-  static const String appName = 'Flutter Starter';
-  static const String baseUrl = 'https://jsonplaceholder.typicode.com';
-  static const double desktopScaleWidth = 1920;
+  static const String appName = 'Profiler';
+  static const String baseUrl = 'http://127.0.0.1:8000/';
+  static const double desktopScaleWidth = 2560;
   static const double mobileScaleWidth = 450;
 }
