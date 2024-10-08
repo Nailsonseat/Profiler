9ac47b7ad683ef63fc5fdc8b25524f247ddc6e48
nailsonseat <aadarshv3@gmail.com>
2024-10-08 12:24:20 +0530

    register chatbot endpoint

--- backend/profiler/urls.py
+++ backend/profiler/urls.py
@@ -1,5 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from .views.chatBot import ChatBotAPIView
 from .views.userProfile import UserProfileViewSet
 
 router = DefaultRouter()
@@ -7,4 +8,5 @@ router.register(r'profiles', UserProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('chatbot/', ChatBotAPIView.as_view()),
 ]
