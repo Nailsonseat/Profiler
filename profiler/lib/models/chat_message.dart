3e56485a3d2b00a371ab9d2d3604fe1e96f1cf72
nailsonseat <aadarshv3@gmail.com>
2024-10-09 17:17:20 +0530

    Chat message schema update

--- profiler/lib/models/chat_message.dart
+++ profiler/lib/models/chat_message.dart
@@ -1,16 +1,14 @@
 class ChatMessage {
   String message;
-  String sender;
-  String receiver;
+  String role;
   String time;
 
-  ChatMessage({required this.message, required this.sender, required this.receiver, required this.time});
+  ChatMessage({required this.message, required this.role, required this.time});
 
   factory ChatMessage.fromJson(Map<String, dynamic> json) {
     return ChatMessage(
       message: json['message'],
-      sender: json['sender'],
-      receiver: json['receiver'],
+      role: json['role'],
       time: json['time'],
     );
   }
@@ -18,8 +16,7 @@ class ChatMessage {
   Map<String, dynamic> toJson() {
     return {
       'message': message,
-      'sender': sender,
-      'receiver': receiver,
+      'role': role,
       'time': time,
     };
   }
