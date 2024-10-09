0b8519707c9e8a05c7c11d8d37a32b07724e4596
nailsonseat <aadarshv3@gmail.com>
2024-10-09 14:45:21 +0530

    Added validators

---
+++ profiler/lib/constants/validators.dart
@@ -0,0 +1,28 @@
+class Validators {
+  static String? emailValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  static String? usernameValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Username is required';
+    }
+    if (value.contains(' ')) {
+      return 'Username cannot contain spaces';
+    }
+    return null;
+  }
+
+  static String? nameValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Name is required';
+    }
+    return null;
+  }
+}
