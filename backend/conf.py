9b09f82660823ac400e1b501f02c2794ba3329fa
nailsonseat <aadarshv3@gmail.com>
2024-10-13 12:39:06 +0530

    config for sphix documentation

---
+++ backend/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import django
+import sys
+import os
+
+project = 'Profiler'
+copyright = '2024, Aadarsh Verma'
+author = 'Aadarsh Verma'
+release = 'v1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_autodoc_typehints'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+sys.path.insert(0, os.path.abspath('./'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'server.settings'
+
+django.setup()
